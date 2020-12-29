ffd1831aea09a0975e9db8d8378492a17092a7c3
inzee86333 <56289715+inzee86333@users.noreply.github.com>
2020-12-29 15:12:35 +0700

    x

--- components/input.js
+++ components/input.js
@@ -52,3 +52,14 @@ export function TextAreaInput({ id, label, placeholder, value, onChange }) {
         </div>
     )
 }
+
+export function ImageInput({ id, label, placeholder, value, onChange }) {
+    return (
+        <div className="md:w-full px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor={id}>
+                {label}
+            </label>
+            <textarea className="appearance-none block w-full text-grey-darker border rounded py-3 px-4 mb-3" id={id} placeholder={placeholder} value={value} onChange={onChange} />
+        </div>
+    )
+}
--- pages/unauthorized/register.js
+++ pages/unauthorized/register.js
@@ -1,4 +1,3 @@
-import Navber from '../../components/navbar'
 import { useState } from 'react'
 import { TextInput, PasswordInput, PhoneNumberInput, EmailInput, TextAreaInput } from '../../components/input'
 import { PrimaryButton } from '../../components/button'
@@ -26,6 +25,15 @@ export default function Register() {
                         สมัครสมาชิก
                     </h1>
                     <form>
+                        <div class="flex w-full h-min items-center py-5 justify-center">
+                            <label class="w-64 flex flex-col font-semibold items-center px-4 py-6 bg-white text-green-600 rounded-lg shadow-lg tracking-wide uppercase border border-green-600 cursor-pointer hover:bg-green-600 hover:text-white">
+                                <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                                </svg>
+                                <span class="mt-2 text-base leading-normal">Select a file</span>
+                                <input type='file' accept="image/*" class="hidden" />
+                            </label>
+                        </div>
                         <div className="-mx-3 md:flex">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <TextInput id="username" label="ชื่อผู้ใช้" placeholder="ชื่อผู้ใช้ของท่าน" value={username}
